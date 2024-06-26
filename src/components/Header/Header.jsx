33f1fd4a10af8b6c08f7074c1890b994cf30f99a
vakeloon <110176977+vakeloon@users.noreply.github.com>
2024-06-26 23:16:14 +0300

    initial

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from "../Button/Button";
+import {useTelegram} from "../../hooks/useTelegram";
+
+const Header = () => {
+    const {user, onClose} = useTelegram();
+
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose}>Close</Button>
+            <span className={'username'}>
+                {user?.username}
+            </span>
+        </div>
+    );
+};
+
+export default Header;
