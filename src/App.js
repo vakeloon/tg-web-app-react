08e4d9acab186da10810b9656c9675cfa472f88d
vakeloon <110176977+vakeloon@users.noreply.github.com>
2024-06-27 09:56:54 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
+import {Route, Routes} from "react-router-dom";
+import FormWheel from "./components/FormWheel/FormWheel";
 
 
 function App() {
@@ -14,7 +16,9 @@ function App() {
   return (
     <div className="App">
         <Header/>
-        <button onClick={onToggleButton}>toggle</button>
+        <Routes>
+            <Route index element={<FormWheel />} />
+        </Routes>
     </div>
   );
 }
---
+++ src/components/FormWheel/FormWheel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FormWheel.css'
+
+const FormWheel = () => {
+    return (
+        <div>
+            FORM WHEEL
+        </div>
+    );
+};
+
+export default FormWheel;
