c41652cc3bb5ebf35705a71e088fff5209316ad0
vakeloon <110176977+vakeloon@users.noreply.github.com>
2024-06-27 10:06:32 +0300

    init

--- src/components/FormWheel/FormWheel.jsx
+++ src/components/FormWheel/FormWheel.jsx
@@ -3,8 +3,25 @@ import './FormWheel.css'
 
 const FormWheel = () => {
     return (
-        <div>
-            FORM WHEEL
+        <div className={"formWheel"}>
+            <div className={"colors"}>
+                <h1>Guess the color</h1>
+                <button className={"color-button red"} data-color="red"></button>
+                <button className={"color-button orange"} data-color="orange"></button>
+                <button className={"color-button yellow"} data-color="yellow"></button>
+                <button className={"color-button blue"} data-color="blue"></button>
+                <button className={"color-button purple"} data-color="purple"></button>
+                <button className={"color-button green"} data-color="green"></button>
+            </div>
+            <div className="wheel-container">
+                <img id="color-wheel" src="./color-wheel.png" alt="Color Wheel"/>
+                <img className={"arrow"} src="./right-arrow.png" alt="Arrow"/>
+            </div>
+            <button id="spin-button">Spin the wheel</button>
+            <div id="result-message">
+                <div id="color-message"></div>
+                <div id="prize-message"></div>
+            </div>
         </div>
     );
 };
